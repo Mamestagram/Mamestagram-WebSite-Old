1db4e93b838db0dddabfb0b67b59a0e3f22e733b
basshhii0610 <117554796+basshhii0610@users.noreply.github.com>
2023-04-26 06:18:29 +0900

    スマホ用のヘッダーメニュー追加

--- public/jquery/header.js
+++ public/jquery/header.js
@@ -1,6 +1,8 @@
 $(function() {
     $("body").click(function(e) {
         var $form = $(".login-form, .link-form");
+        var $mobile = $(".header-mobile");
+        var $arrow = $(".header .arrow");
         if (!$(e.target).closest(".login, .playername").length && !$(e.target).closest(".login-form").length) {
             $form.removeClass("show");
             $form.slideUp(100);
@@ -16,6 +18,23 @@ $(function() {
             else {
                 $form.addClass("show");
                 $form.slideDown(100);
+            }
+        }
+        if (!$(e.target).closest(".header .arrow").length && !$(e.target).closest($mobile).length) {
+            $mobile.removeClass("show");
+            $mobile.slideUp(100);
+        }
+        else if ($(e.target).closest($mobile).length) {
+            $mobile.show();
+        }
+        else {
+            if ($mobile.hasClass("show")) {
+                $mobile.removeClass("show");
+                $mobile.slideUp(100);
+            }
+            else {
+                $mobile.addClass("show");
+                $mobile.slideDown(100);
             }
         }
     });
