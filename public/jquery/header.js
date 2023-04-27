c9bf7c641be90eee0858af72fb0a066768149aef
basshhii0610 <117554796+basshhii0610@users.noreply.github.com>
2023-04-27 21:27:41 +0900

    スマホ対応完成

--- public/jquery/header.js
+++ public/jquery/header.js
@@ -5,11 +5,11 @@ $(function() {
         var $arrow = $(".header .arrow button");
 
         //ログインボタンクリック時
-        if (!$(e.target).closest(".login, .playername").length && !$(e.target).closest(".login-form").length) {
+        if (!$(e.target).closest(".header .login, .header .playername").length && !$(e.target).closest($form).length) {
             $form.removeClass("show");
             $form.slideUp(100);
         }
-        else if ($(e.target).closest(".login-form").length) {
+        else if ($(e.target).closest($form).length) {
             $form.show();
         }
         else {
@@ -20,6 +20,42 @@ $(function() {
             else {
                 $form.addClass("show");
                 $form.slideDown(100);
+            }
+        }
+
+        //ログインボタンクリック時（スマホ）
+        if (!$(e.target).closest(".header-mobile .login, .header-mobile .playername").length && !$(e.target).closest($form).length) {
+            //処理なし
+        }
+        else if ($(e.target).closest($form).length) {
+            $form.show();
+        }
+        else {
+            if ($form.hasClass("show")) {
+                $form.removeClass("show");
+                $form.slideUp(100);
+                $mobile.addClass("show");
+                $mobile.slideDown(100);
+                $arrow.css({
+                    "top": "5px",
+                    "bottom": "0",
+                    "border-top": "3px solid #c1b7df",
+                    "border-right": "3px solid #c1b7df",
+                    "transform": "rotate(315deg)"
+                });
+            }
+            else {
+                $form.addClass("show");
+                $form.slideDown(100);
+                $mobile.removeClass("show");
+                $mobile.slideUp(100);
+                $arrow.css({
+                    "top": "0",
+                    "bottom": "5px",
+                    "border-top": "3px solid #fdfdfd",
+                    "border-right": "3px solid #fdfdfd",
+                    "transform": "rotate(135deg)"
+                });
             }
         }
 
