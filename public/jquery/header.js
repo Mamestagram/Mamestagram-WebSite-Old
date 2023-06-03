b685febe512c84d263362d89b41e4dfcd958db4d
basshhii0610 <117554796+basshhii0610@users.noreply.github.com>
2023-06-03 21:51:45 +0900

    コード書き換え

--- public/jquery/header.js
+++ public/jquery/header.js
@@ -1,11 +1,10 @@
 $(function() {
     $("body").click(function(e) {
         var $form = $(".login-form, .link-form");
-        var $mobile = $(".header-mobile");
-        var $arrow = $(".header .arrow button");
+        var $mobile = $(".mobile");
 
         //ログインボタンクリック時
-        if (!$(e.target).closest(".header .login, .header .playername").length && !$(e.target).closest($form).length) {
+        if (!$(e.target).closest("header .login, header .playername").length && !$(e.target).closest($form).length) {
             $form.removeClass("show");
             $form.slideUp(100);
         }
@@ -24,52 +23,26 @@ $(function() {
         }
 
         //ログインボタンクリック時（スマホ）
-        if (!$(e.target).closest(".header-mobile .login, .header-mobile .playername").length && !$(e.target).closest($form).length) {
+        if (!$(e.target).closest(".mobile .login, .mobile .playername").length && !$(e.target).closest($form).length) {
             //処理なし
         }
         else if ($(e.target).closest($form).length) {
             $form.show();
         }
         else {
-            if ($form.hasClass("show")) {
-                $form.removeClass("show");
-                $form.slideUp(100);
-                $mobile.addClass("show");
-                $mobile.slideDown(100);
-                $arrow.css({
-                    "top": "5px",
-                    "bottom": "0",
-                    "border-top": "3px solid #c1b7df",
-                    "border-right": "3px solid #c1b7df",
-                    "transform": "rotate(315deg)"
-                });
-            }
-            else {
-                $form.addClass("show");
-                $form.slideDown(100);
-                $mobile.removeClass("show");
-                $mobile.slideUp(100);
-                $arrow.css({
-                    "top": "0",
-                    "bottom": "5px",
-                    "border-top": "3px solid #fdfdfd",
-                    "border-right": "3px solid #fdfdfd",
-                    "transform": "rotate(135deg)"
-                });
-            }
-        }
-
-        //矢印クリック時
-        if (!$(e.target).closest(".header .arrow").length && !$(e.target).closest($mobile).length) {
+            $form.slideDown(100);
             $mobile.removeClass("show");
             $mobile.slideUp(100);
-            $arrow.css({
-                "top": "0",
-                "bottom": "5px",
-                "border-top": "3px solid #fdfdfd",
-                "border-right": "3px solid #fdfdfd",
-                "transform": "rotate(135deg)"
-            });
+            $(".standard .down").show();
+            $(".standard .up").hide();
+        }
+
+        //矢印クリック時（スマホ）
+        if (!$(e.target).closest("header .arrow").length && !$(e.target).closest($mobile).length) {
+            $mobile.removeClass("show");
+            $mobile.slideUp(100);
+            $(".standard .down").show();
+            $(".standard .up").hide();
         }
         else if ($(e.target).closest($mobile).length) {
             $mobile.show();
@@ -78,24 +51,14 @@ $(function() {
             if ($mobile.hasClass("show")) {
                 $mobile.removeClass("show");
                 $mobile.slideUp(100);
-                $arrow.css({
-                    "top": "0",
-                    "bottom": "5px",
-                    "border-top": "3px solid #fdfdfd",
-                    "border-right": "3px solid #fdfdfd",
-                    "transform": "rotate(135deg)"
-                });
+                $(".standard .down").show();
+                $(".standard .up").hide();
             }
             else {
                 $mobile.addClass("show");
                 $mobile.slideDown(100);
-                $arrow.css({
-                    "top": "5px",
-                    "bottom": "0",
-                    "border-top": "3px solid #c1b7df",
-                    "border-right": "3px solid #c1b7df",
-                    "transform": "rotate(315deg)"
-                });
+                $(".standard .down").hide();
+                $(".standard .up").show();
             }
         }
     });
