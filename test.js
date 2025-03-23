4784dd8e9866239f3286363e9423f49a0bde69bb
basshhii0610 <kachakun0610@gmail.com>
2025-03-23 09:53:01 +0900

    プロフ修正

--- test.js
+++ test.js
@@ -1,8 +1,13 @@
-let x = 31767;
-x -= 1 << 4;
-console.log(x);
-for (let i = 0; Math.pow(2, i) <= x; i++) {
-    if ((x & 1 << i) > 0) {
-        console.log(i);
-    }
-}
+const time = new Date(Date.now());
+const out = time.toLocaleString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    timeZone: "Australia/Hugh_Henry",
+    timeZoneName: "short",
+    language: "en"
+});
+console.log(out);
