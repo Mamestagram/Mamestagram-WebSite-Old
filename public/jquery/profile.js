c3025496676b94798a57a9a56c58d5383015e3ec
basshhii0610 <kachakun0610@gmail.com>
2025-06-18 14:55:01 +0900

    profileページ読込速度改善

--- public/jquery/profile.js
+++ public/jquery/profile.js
@@ -5,110 +5,15 @@ $(window).resize(function() {
         wid -= 3 + 30 + 10 + $("main .member .member-table .data .country").eq(0).width() + $("main .member .member-table .data .acc").eq(0).width() + $("main .member .member-table .data .plays").eq(0).width() + $("main .member .member-table .data .pp").eq(0).width();
         $("main .member .member-table .data .username").css("width", `${wid}px`);
     }
-
-    if ($("body").width() <= 767) {
-        let hei = 35;
-        for (let i = 0; i < Math.min(5, $("main .bestpp div .data-mobile").length); i++) {
-            hei += $("main .bestpp div .data-mobile").eq(i).height() + 5;
-        }
-        hei = Math.max(452, hei);
-        $("main .bestpp").css("max-height", `${hei}px`);
-        hei = 35;
-        for (let i = 0; i < Math.min(5, $("main .recentplays div .data-mobile-passed").length); i++) {
-            hei += $("main .recentplays div .data-mobile-passed").eq(i).height() + 5;
-        }
-        hei = Math.max(452, hei);
-        $("main .recentplays").css("max-height", `${hei}px`);
-        $("main .recentplays .data-mobile").hide();
-        $("main .recentplays .data-mobile-passed").show();
-        hei = 35;
-        for (let i = 0; i < Math.min(5, $("main .first_place div .data-mobile").length); i++) {
-            hei += $("main .first_place div .data-mobile").eq(i).height() + 5;
-        }
-        hei = Math.max(452, hei);
-        $("main .first_place").css({
-            "min-height": `${hei}px`,
-            "max-height": `${hei}px`
-        });
-    }
-    else {
-        $("main .bestpp, main .mostplays, main .recentplays, main .first_place").css({
-            "min-height": "",
-            "max-height": ""
-        });
-    }
-    $("main .show-more.bp").each(function(i) {
-        if (i === 0) {
-            $(this).show();
-        }
-        else {
-            $(this).hide();
-        }
-    });
-    $("main .show-more.mp").each(function(i) {
-        if (i === 0) {
-            $(this).show();
-        }
-        else {
-            $(this).hide();
-        }
-    });
-    $("main .show-more.rp").each(function(i) {
-        if (i === 0) {
-            $(this).show();
-        }
-        else {
-            $(this).hide();
-        }
-    });
-    $("main .show-more.fp").each(function(i) {
-        if (i === 0) {
-            $(this).show();
-        }
-        else {
-            $(this).hide();
-        }
-    });
 });
 
 $(function() {
-    let rs = "passed";
     if ($("body").width() <= 1232) {
         var $table = $("main .member .member-table");
         var $data = $("main .member .member-table .data").eq(0);
         let wid = $table.width();
         wid -= 3 + 30 + 10 + $data.children(".country").width() + $data.children(".acc").width() + $data.children(".plays").width() + $data.children(".pp").width();
         $("main .member .member-table .data .username").css("width", `${wid}px`);
-    }
-
-    if ($("body").width() <= 767) {
-        let hei = 35;
-        for (let i = 0; i < Math.min(5, $("main .bestpp div .data-mobile").length); i++) {
-            hei += $("main .bestpp div .data-mobile").eq(i).height() + 5;
-        }
-        hei = Math.max(452, hei);
-        $("main .bestpp").css({
-            "min-height": `${hei}px`,
-            "max-height": `${hei}px`
-        });
-        hei = 35;
-        for (let i = 0; i < Math.min(5, $("main .recentplays div .data-mobile-passed").length); i++) {
-            hei += $("main .recentplays div .data-mobile-passed").eq(i).height() + 5;
-        }
-        hei = Math.max(452, hei);
-        $("main .recentplays").css({
-            "min-height": `${hei}px`,
-            "max-height": `${hei}px`
-        });
-        hei = 35;
-        for (let i = 0; i < Math.min(5, $("main .first_place div .data-mobile").length); i++) {
-            hei += $("main .first_place div .data-mobile").eq(i).height() + 5;
-        }
-        hei = Math.max(452, hei);
-        $("main .first_place").css({
-            "min-height": `${hei}px`,
-            "max-height": `${hei}px`
-        });
     }
 
     // relationshipクリック
@@ -303,124 +208,8 @@ $(function() {
         textarea.setSelectionRange(nextCursorPos, nextCursorPos);
     });
 
-    // "show more"をクリックしたとき
-    $("main .show-more").click(function() {
-        var $selection, $nextbtn, $subject;
-        let hei, amt = 50;
-
-        while (!$(this).hasClass(`to${amt}`)) {
-            amt += 50;
-        }
-        if ($(this).hasClass("bp")) {
-            $selection = $("main .bestpp");
-            $nextbtn = $(`main .bp.to${amt + 50}`);
-            $subject = $("main .bestpp div .data-mobile");
-        }
-        else if ($(this).hasClass("mp")) {
-            $selection = $("main .mostplays");
-            $nextbtn = $(`main .mp.to${amt + 50}`);
-        }
-        else if ($(this).hasClass("rp")) {
-            $selection = $("main .recentplays");
-            $nextbtn = $(`main .rp${$(this).hasClass("nof") ? ".nof" : ""}.to${amt + 50}`);
-            $subject = $(`main .recentplays div .data-mobile${rs === "passed" ? "-passed" : ""}`);
-        }
-        else if ($(this).hasClass("fp")) {
-            $selection = $("main .first_place");
-            $nextbtn = $(`main .mp.to${amt + 50}`);
-            $subject = $("main .first_place div .data-mobile");
-        }
-        if ($("body").width() <= 767) {
-            hei = 35;
-            if ($subject !== undefined) {
-                for (let i = 0; i < Math.min(amt, $subject.length); i++) {
-                    hei += $subject.eq(i).height() + 5;
-                }
-            }
-            else {
-                hei += 59 * amt;
-            }
-        }
-        else {
-            hei = 50 + 60 * amt;
-        }
-        $selection.css("max-height", `${hei}px`);
-        $(this).hide();
-        $nextbtn.show();
-    });
-
     $("main .verified").hover(function() {
         $(this).hide();
         $("main .verified-hover").show();
-    });
-
-    // "Show all"をクリックしたとき
-    $("main .recentplays .all").click(function() {
-        rs = "all";
-        $("main .recentplays div .nothing").show();
-        $("main .recentplays div .nothing-passed").hide();
-        $(this).css({
-            "filter": `invert(${0}%)`,
-            "opacity": 1
-        });
-        $("main .recentplays .passed").css({
-            "filter": "",
-            "opacity": ""
-        });
-        if ($("body").width() > 767) {
-            $("main .recentplays").css("max-height", `${350}px`);
-            $("main .recentplays .data").show();
-            $("main .recentplays .data-passed").hide();
-        }
-        else {
-            let hei = 35;
-            for (let i = 0; i < Math.min(5, $("main .recentplays div .data-mobile").length); i++) {
-                hei += $("main .recentplays div .data-mobile").eq(i).height() + 5;
-            }
-            hei = Math.max(452, hei);
-            $("main .recentplays").css({
-                "min-height": `${hei}px`,
-                "max-height": `${hei}px`
-            });
-            $("main .recentplays .data-mobile").show();
-            $("main .recentplays .data-mobile-passed").hide();
-        }
-        $("main .rp.to50").show();
-        $("main .rp.nof").hide();
-    });
-
-    // "Only passed"をクリックしたとき
-    $("main .recentplays .passed").click(function() {
-        rs = "passed";
-        $(this).css({
-            "filter": `invert(${0}%)`,
-            "opacity": 1
-        });
-        $("main .recentplays .all").css({
-            "filter": "",
-            "opacity": ""
-        });
-        $("main .recentplays div .nothing").hide();
-        $("main .recentplays div .nothing-passed").show();
-        if ($("body").width() > 767) {
-            $("main .recentplays").css("max-height", `${350}px`);
-            $("main .recentplays .data").hide();
-            $("main .recentplays .data-passed").show();
-        }
-        else {
-            let hei = 35;
-            for (let i = 0; i < Math.min(5, $("main .recentplays div .data-mobile-passed").length); i++) {
-                hei += $("main .recentplays div .data-mobile-passed").eq(i).height() + 5;
-            }
-            hei = Math.max(452, hei);
-            $("main .recentplays").css({
-                "min-height": `${hei}px`,
-                "max-height": `${hei}px`
-            });
-            $("main .recentplays .data-mobile").hide();
-            $("main .recentplays .data-mobile-passed").show();
-        }
-        $("main .rp").hide();
-        $("main .rp.nof.to50").show();
     });
 });
